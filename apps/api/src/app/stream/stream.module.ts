6176a526270a7300f79afc73c821f581de2977b9
Aviv Haruzi <avivharuzi@gmail.com>
2022-04-14 19:30:52 +0300

    fix: 🐛 missing stream service provider

--- apps/api/src/app/stream/stream.module.ts
+++ apps/api/src/app/stream/stream.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { VideosModule } from '../videos';
 import { StreamPathPipe } from './stream-path.pipe';
 import { StreamController } from './stream.controller';
+import { StreamService } from './stream.service';
 
 @Module({
   imports: [VideosModule],
   controllers: [StreamController],
-  providers: [StreamPathPipe],
+  providers: [StreamPathPipe, StreamService],
 })
 export class StreamModule {}
