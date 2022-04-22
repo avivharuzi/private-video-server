db53c82c2418ee07c6f812e7ae6544acf5fe3275
Aviv Haruzi <avivharuzi@gmail.com>
2022-04-22 19:16:19 +0300

    feat: 🎸 add user environment variables

--- apps/api/src/app/environment-variables.ts
+++ apps/api/src/app/environment-variables.ts
@@ -1,4 +1,7 @@
 export interface EnvironmentVariables {
   API_PORT: number;
   API_MEDIA_DIRECTORY: string;
+  API_JWT_SECRET: string;
+  API_ADMIN_USERNAME: string;
+  API_ADMIN_PASSWORD: string;
 }
