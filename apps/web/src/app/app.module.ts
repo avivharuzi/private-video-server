416c685f49983493f3e8c2fb37ec85c6f63ca65d
Aviv Haruzi <avivharuzi@gmail.com>
2022-05-06 16:18:41 +0300

    feat: 🎸 add BrowserAnimationsModule

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { CollectionsDataAccessModule } from '@private-video-server/collections/data-access';
@@ -18,6 +19,7 @@ import { NavbarModule } from './navbar/navbar.module';
   imports: [
     AppRoutingModule,
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserAnimationsModule,
     HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
