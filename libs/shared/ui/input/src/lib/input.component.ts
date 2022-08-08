52ed8b79d50e78f105932fa943ed818a07fb033a
Aviv Haruzi <avivharuzi@gmail.com>
2022-08-08 22:55:29 +0300

    refactor: 💡 update input

--- libs/shared/ui/input/src/lib/input.component.ts
+++ libs/shared/ui/input/src/lib/input.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'input[sharedInput]',
+  selector: 'input[sharedInput],select[sharedInput]',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
