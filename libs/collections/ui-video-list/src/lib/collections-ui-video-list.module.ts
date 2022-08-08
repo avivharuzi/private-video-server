6669507611430c654c8d6ce530c57ac225e9559a
Aviv Haruzi <avivharuzi@gmail.com>
2022-08-08 23:14:14 +0300

    feat: 🎸 add filtering to video list

--- libs/collections/ui-video-list/src/lib/collections-ui-video-list.module.ts
+++ libs/collections/ui-video-list/src/lib/collections-ui-video-list.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { CollectionsUiStreamModule } from '@private-video-server/collections/ui-stream';
 import { SharedUiActionMenuModule } from '@private-video-server/shared/ui/action-menu';
 import { SharedUiButtonModule } from '@private-video-server/shared/ui/button';
 import { SharedUiIconModule } from '@private-video-server/shared/ui/icon';
+import { SharedUiInputModule } from '@private-video-server/shared/ui/input';
 import { SharedUiLoaderModule } from '@private-video-server/shared/ui/loader';
 import { SharedUiVideoDurationModule } from '@private-video-server/shared/ui/video-duration';
 import { SharedUiVideoQualityModule } from '@private-video-server/shared/ui/video-quality';
@@ -17,10 +19,12 @@ import { VideoListComponent } from './video-list.component';
   imports: [
     CommonModule,
     RouterModule,
+    ReactiveFormsModule,
     CollectionsUiStreamModule,
     SharedUiActionMenuModule,
     SharedUiButtonModule,
     SharedUiIconModule,
+    SharedUiInputModule,
     SharedUiLoaderModule,
     SharedUiVideoQualityModule,
     SharedUiVideoDurationModule,
--- libs/collections/ui-video-list/src/lib/video-list.component.ts
+++ libs/collections/ui-video-list/src/lib/video-list.component.ts
@@ -1,6 +1,26 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnDestroy,
+} from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
-import { Video } from '@private-video-server/collections/data-access';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  Subject,
+  takeUntil,
+  tap,
+} from 'rxjs';
+
+import {
+  getVideoSortByOptions,
+  Video,
+  VideoSortBy,
+} from '@private-video-server/collections/data-access';
+import { deepClone } from '@private-video-server/shared/util-helpers';
 
 @Component({
   selector: 'collections-video-list',
@@ -8,10 +28,102 @@ import { Video } from '@private-video-server/collections/data-access';
   styleUrls: ['./video-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class VideoListComponent {
-  @Input() videos: Video[] = [];
+export class VideoListComponent implements OnDestroy {
+  @Input()
+  set videos(videos: Video[]) {
+    this._videos = deepClone(videos);
+    this.sourceVideos = deepClone(videos);
+    this.updateVideosByFilter();
+  }
+
+  get videos(): Video[] {
+    return this._videos;
+  }
+
+  videoSortByOptions = getVideoSortByOptions();
+
+  videoListFilterForm = this.formBuilder.group({
+    searchTerm: this.formBuilder.control(''),
+    sortBy: this.formBuilder.control(VideoSortBy.CreationDateDesc),
+  });
+
+  private _videos: Video[] = [];
+
+  private sourceVideos: Video[] = [];
+
+  private destroySubject = new Subject<void>();
+
+  constructor(
+    private readonly changeDetectorRef: ChangeDetectorRef,
+    private readonly formBuilder: FormBuilder
+  ) {
+    this.listenToVideoListFilterFormValueChanges();
+  }
+
+  ngOnDestroy(): void {
+    this.destroySubject.next();
+  }
 
   onVideoDeleted(deletedVideo: Video): void {
-    this.videos = this.videos.filter((video) => video.id !== deletedVideo.id);
+    this.videos = this.sourceVideos.filter(
+      (video) => video.id !== deletedVideo.id
+    );
+  }
+
+  private listenToVideoListFilterFormValueChanges(): void {
+    this.videoListFilterForm.valueChanges
+      .pipe(
+        takeUntil(this.destroySubject),
+        debounceTime(300),
+        distinctUntilChanged(),
+        tap(() => {
+          this.updateVideosByFilter();
+        })
+      )
+      .subscribe();
+  }
+
+  private updateVideosByFilter(): void {
+    const { searchTerm, sortBy } = this.videoListFilterForm.value;
+    const searchTermTrim = searchTerm.trim();
+
+    let videos: Video[];
+
+    if (searchTermTrim === '') {
+      videos = deepClone(this.sourceVideos);
+    } else {
+      videos = this.sourceVideos.filter(
+        (video) =>
+          video.title.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1
+      );
+    }
+
+    switch (sortBy) {
+      case VideoSortBy.CreationAtAsc:
+        videos = videos.sort(
+          (a, b) =>
+            new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+        );
+
+        break;
+      case VideoSortBy.CreationDateDesc:
+        videos = videos.sort(
+          (a, b) =>
+            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+        );
+
+        break;
+      case VideoSortBy.TitleDesc:
+        videos = videos.sort((a, b) => b.title.localeCompare(a.title));
+
+        break;
+      case VideoSortBy.TitleAsc:
+        videos = videos.sort((a, b) => a.title.localeCompare(b.title));
+
+        break;
+    }
+
+    this._videos = videos;
+    this.changeDetectorRef.detectChanges();
   }
 }
