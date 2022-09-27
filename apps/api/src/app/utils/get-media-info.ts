18602433478a60ba0f7695ac20f73c077bf16fbb
Aviv Haruzi <avivharuzi@gmail.com>
2022-09-27 12:09:05 +0300

    feat: 🎸 add get media info function

---
+++ apps/api/src/app/utils/get-media-info.ts
@@ -0,0 +1,102 @@
+import * as fs from 'node:fs';
+
+import * as MediaInfoFactory from 'mediainfo.js';
+import { FormatType, MediaInfo, ReadChunkFunc, Result } from 'mediainfo.js';
+
+export interface MediaInfoOptions {
+  coverData: boolean;
+  format: FormatType;
+  full: boolean;
+}
+
+export const getDefaultMediaInfoOptions = (): MediaInfoOptions => {
+  return {
+    coverData: false,
+    format: 'JSON',
+    full: false,
+  };
+};
+
+export type MediaInfoFormatted = Record<string, Record<string, string>>;
+
+export const getMediaInfo = async (
+  inputFilePath: string,
+  options: Partial<MediaInfoOptions> = {}
+) => {
+  const mergedOptions: MediaInfoOptions = {
+    ...getDefaultMediaInfoOptions(),
+    ...options,
+  };
+
+  let fileHandle: fs.promises.FileHandle | undefined;
+  let fileSize: number;
+  let mediainfo: MediaInfo | undefined;
+  let result: Result;
+
+  const readChunk: ReadChunkFunc = async (size, offset) => {
+    const buffer = new Uint8Array(size);
+    await (fileHandle as fs.promises.FileHandle).read(buffer, 0, size, offset);
+    return buffer;
+  };
+
+  try {
+    fileHandle = await fs.promises.open(inputFilePath, 'r');
+    fileSize = (await fileHandle.stat()).size;
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    mediainfo = await MediaInfoFactory(mergedOptions);
+    result = (await mediainfo?.analyzeData(
+      () => fileSize,
+      readChunk
+    )) as Result;
+  } finally {
+    fileHandle && (await fileHandle.close());
+    mediainfo && mediainfo.close();
+  }
+
+  return result;
+};
+
+export const getMediaInfoFormatted = async (
+  inputFilePath: string
+): Promise<MediaInfoFormatted> => {
+  const text = (await getMediaInfo(inputFilePath, {
+    format: 'text',
+  })) as string;
+
+  let hasToCheckGroup = true;
+  let lastGroupName = '';
+  const result: MediaInfoFormatted = {};
+
+  const lines = text.split('\n');
+
+  lines.forEach((line) => {
+    const lineText = line.trim();
+
+    if (lineText === '') {
+      hasToCheckGroup = true;
+      return;
+    }
+
+    if (hasToCheckGroup) {
+      lastGroupName = lineText;
+      result[lastGroupName] = {};
+      hasToCheckGroup = false;
+      return;
+    }
+
+    const textValues = lineText.split(': ');
+
+    if (textValues.length !== 2) {
+      return;
+    }
+
+    const key = (textValues[0] as string).trim();
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    result[lastGroupName][key] = (textValues[1] as string).trim();
+  });
+
+  return result;
+};
--- apps/api/src/app/utils/index.ts
+++ apps/api/src/app/utils/index.ts
@@ -4,6 +4,7 @@ export * from './create-full-video-preview';
 export * from './create-video-preview';
 export * from './generate-uuid';
 export * from './get-file-number';
+export * from './get-media-info';
 export * from './get-video-info';
 export * from './is-file-exists';
 export * from './shrink-image';
