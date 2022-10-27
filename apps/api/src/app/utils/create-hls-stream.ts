dfd5885b3000cd4693e007877fe143c16b023833
Aviv Haruzi <avivharuzi@gmail.com>
2022-10-27 18:21:25 +0300

    feat: 🎸 add createHLSStream and recreateDirectory utils

---
+++ apps/api/src/app/utils/create-hls-stream.ts
@@ -0,0 +1,38 @@
+import * as path from 'node:path';
+
+import * as ffmpeg from 'fluent-ffmpeg';
+
+export interface CreateHLSStreamOptions {
+  name: string;
+  targetDir: string;
+}
+
+export const createHLSStream = (
+  inputFilePath: string,
+  { name, targetDir }: CreateHLSStreamOptions
+): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const m3u8FileName = `${name}-master.m3u8`;
+    const m3u8FilePath = path.join(targetDir, m3u8FileName);
+    const hlsSegmentFilename = path.join(targetDir, `${name}-_%03d.ts`);
+
+    ffmpeg()
+      .input(inputFilePath)
+      .inputOptions(['-hide_banner', '-y'])
+      .outputOptions([
+        '-c copy',
+        '-sc_threshold 0',
+        '-g 48',
+        '-keyint_min 48',
+        '-hls_time 2',
+        '-hls_playlist_type vod',
+        `-hls_segment_filename ${hlsSegmentFilename}`,
+      ])
+      .output(m3u8FilePath)
+      .on('end', () => {
+        resolve(m3u8FileName);
+      })
+      .on('error', reject)
+      .run();
+  });
+};
---
+++ apps/api/src/app/utils/recreate-directory.ts
@@ -0,0 +1,21 @@
+import * as fs from 'node:fs';
+
+export const recreateDirectory = async (
+  targetDirectory: string
+): Promise<void> => {
+  let isExist = true;
+
+  try {
+    await fs.promises.access(targetDirectory, fs.constants.R_OK);
+  } catch (_) {
+    isExist = false;
+  }
+
+  if (isExist) {
+    await fs.promises.rm(targetDirectory, { recursive: true, force: true });
+  }
+
+  await fs.promises.mkdir(targetDirectory, {
+    recursive: true,
+  });
+};
